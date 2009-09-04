0609e702975cab56b4b72c9090fdfe96ca3a1dbc
David Ibach <david.ibach@ou.edu>
2009-09-04 15:05:44 -0500

    fully operational program

--- HW1/hw1.asm
+++ HW1/hw1.asm
@@ -9,6 +9,7 @@ BAUD			=	$102B
 
 BUFFER_BEG		=	$0190		//Starting Point of Buffer
 BUFFER_END		=	$01CC		//End of buffer Store 3C for 60
+STORE			=	$01CE
 
 
 **************
@@ -24,6 +25,12 @@ Loop:			ldx	#Prompt
 			jsr	SCI_OUT_MSG
 			ldx	#BUFFER_BEG
 			jsr	SCI_IN_MSG
+			ldx	#Answer
+			jsr	SCI_OUT_MSG
+			ldx	#BUFFER_BEG
+			jsr	SCI_OUT_MSG
+			ldx	#CR
+			jsr	SCI_OUT_MSG
 			bra	Loop
 
 Name:			fcc	"David Ibach & Christoph Koehler"
@@ -31,6 +38,11 @@ Name:			fcc	"David Ibach & Christoph Koehler"
 
 Prompt:		fcc	"Enter a message: "
 			fcb	0
+
+Answer:		fcc	"	After ROT13: "
+			fcb	0
+
+CR:			fcb	13,10,0
 
 
 
@@ -79,14 +91,67 @@ SCI_IN_MSG_1:	ldaa	SCSR
 			beq	SCI_IN_MSG_END
 			cpx	#BUFFER_END
 			beq	SCI_IN_MSG_1
-			staa	0,x
-			inx
 			jsr	SCI_Char_OUT
+			jsr	ROT13_CYPHER
+			staa	0,x
+			inx	
+			ldaa	#$00
+			staa	0,x
 			bra	SCI_IN_MSG_1
-SCI_IN_MSG_END:	ldaa #$0D
-			jsr SCI_Char_OUT
+SCI_IN_MSG_END:	ldaa	#$0D
+			jsr	SCI_Char_OUT
 			pula
 			rts
+
+ROT13_CYPHER:	pshb
+			TAB
+			andb	#$40
+			cmpb	#$40
+			bne	ROT13_CYPHER_END
+			TAB
+			andb	#$5C
+			cmpb	#$5C
+			beq	ROT13_CYPHER_END
+			TAB	
+			andb	#$4F
+			cmpb	#$40
+			beq	ROT13_CYPHER_END
+			TAB
+			andb	#$5B
+			cmpb	#$5B
+			beq	ROT13_CYPHER_END
+			TAB
+			andb	#$20
+			beq	ROT13_CYPHER_UP
+			bra	ROT13_CYPHER_LOW
+ROT13_CYPHER_UP:	TAB	
+			adda	#13
+			addb	#13
+			subb	#$5B
+			stab	STORE
+			comb	
+			andb	#$80
+			cmpb	#$80
+			bne	ROT13_CYPHER_END
+			ldaa	#'A'
+			adda	STORE	
+			bra	ROT13_CYPHER_END
+ROT13_CYPHER_LOW:	TAB	
+			adda	#13
+			addb	#13
+			subb	#$7B
+			stab	STORE
+			comb	
+			andb	#$80
+			cmpb	#$80
+			bne	ROT13_CYPHER_END
+			ldaa	#'a'
+			adda	STORE	
+			bra	ROT13_CYPHER_END
+ROT13_CYPHER_END:	pulb
+			rts
+
+			
 
 			org	$FFFE
 			dc.w	Init
